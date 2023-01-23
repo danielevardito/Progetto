55aacdfc7aeede0d43c336df4b4119dcb2a9c15e
danielevardito <danielevito.ardito@studio.unibo.it>
2023-01-23 17:12:55 +0100

    0. Commit di inizio progetto

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <ncurses.h>
+#include <string>
+
+using namespace std;
+
+/*
+MAIN DEL PROGETTO, coordina tutte le classi e
+permette il funzionamento del programma.
+*/
+
+int main(){
+    return 0;
+}
